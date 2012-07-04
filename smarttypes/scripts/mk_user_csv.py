ff2c251a11ca023b5f68fc141bdf07c9fb3a9e13
Timmyt Wilson <hello@smarttypes.org>
2012-07-04 10:34:18 -0400

    blah

--- smarttypes/scripts/mk_user_csv.py
+++ smarttypes/scripts/mk_user_csv.py
@@ -3,20 +3,21 @@ import smarttypes, sys
 from smarttypes.model.twitter_user import TwitterUser
 from smarttypes.model.twitter_tweet import TwitterTweet
 from datetime import datetime, timedelta
-
 from smarttypes.utils.postgres_handle import PostgresHandle
-postgres_handle = PostgresHandle(smarttypes.connection_string)
+
 
 if __name__ == "__main__":
+
+	postgres_handle = PostgresHandle(smarttypes.connection_string)
 
     if not len(sys.argv) > 1:
         raise Exception('Need a twitter handle.')
     else:
         screen_name = sys.argv[1]
         
-    #friends
+    root_user = TwitterUser.by_screen_name(screen_name, postgres_handle)
     friends_file = open('/tmp/%s_twitter_friends.csv' % screen_name, 'w')
-    TwitterUser.mk_following_following_csv(screen_name, friends_file, postgres_handle)
+    TwitterUser.mk_following_following_csv(root_user.id, friends_file, postgres_handle)
     
     #tweets_file = open('/tmp/%s_twitter_tweets.csv')
     #TwitterUser.mk_following_tweets_csv(screen_name, tweets_file)
