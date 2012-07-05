7769900a4806d8098790b6eaea67baf4a8fcbe74
Timmyt Wilson <hello@smarttypes.org>
2012-07-05 07:06:17 -0400

    picking a graph clustering algo

--- smarttypes/graphreduce/mk_graph_reduction.py
+++ smarttypes/graphreduce/mk_graph_reduction.py
@@ -9,11 +9,14 @@ from smarttypes.utils.postgres_handle import PostgresHandle
 def print_user_details(user_ids, postgres_handle):
 	for user in TwitterUser.get_by_ids(user_ids, postgres_handle):
 		try:
-			print "%s -- %s" % (user.screen_name, user.description[:100].replace('\n', ' ') 
-				if user.description else '')
+			print "%s -- %s -- %s " % (
+				user.screen_name, 
+				user.location_name,
+				user.description[:100].replace('\n', ' ') if user.description else '',
+			)
 		except Exception, e:
 			""
-			
+
 if __name__ == "__main__":
 
     start_time = datetime.now()
@@ -23,7 +26,7 @@ if __name__ == "__main__":
     else:
         screen_name = sys.argv[1]
     root_user = TwitterUser.by_screen_name(screen_name, postgres_handle)
-    network = TwitterUser.get_rooted_network(root_user, postgres_handle, distance=10)
+    network = TwitterUser.get_rooted_network(root_user, postgres_handle, distance=50)
     g = Graph(directed=True)
     g.add_vertices(network.keys())
     for source in network:
@@ -31,11 +34,25 @@ if __name__ == "__main__":
     		if target in network:
     			g.add_edge(source, target)
     del network
-    communities = g.community_infomap()
 
+    communities = g.community_infomap() #modularity: 0.354382989825
+    #communities = g.community_fastgreedy().as_clustering() #undirected graphs only
+
+    #communities = g.community_label_propagation() #modularity: 0.325206216866
+    #communities = g.community_walktrap().as_clustering() #modularity: 0.417456600391
+    #communities = g.community_spinglass() #modularity: 0.448054107192
+    #communities = g.community_edge_betweenness().as_clustering() #modularity: 0.0039106707787
+
+
+    print "modularity: %s" % communities.modularity
+    print "sizes: %s" % communities.sizes()
+    print "----------------------------------------"
+    print ""
     for community_graph in communities.subgraphs():
-    	print ""
-    	print "----------------------------------------"
-    	member_ids = community_graph.vs["name"]
-    	print_user_details(member_ids, postgres_handle)
-    
+    	dyad_census = community_graph.dyad_census()
+    	if dyad_census[2]:
+    		dyad_score = float(dyad_census[0]) / float(dyad_census[2])
+	    	print "%s : %s" % (len(community_graph.vs), dyad_score)
+	    	member_ids = community_graph.vs["name"]
+	    	print_user_details(member_ids, postgres_handle)
+	    	print "--end---------------------------------------"
