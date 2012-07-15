dbda67f08a231ba2d1c4e19131160a4fa57bb334
Timmyt Wilson <hello@smarttypes.org>
2012-07-15 15:33:15 -0400

    working on the map making process

--- smarttypes/graphreduce/reduce_graph.py
+++ smarttypes/graphreduce/reduce_graph.py
@@ -4,7 +4,7 @@ sys.path.append('/usr/local/lib/python2.7/site-packages')
 from igraph import Graph, plot
 from igraph.clustering import VertexClustering
 from igraph.layout import Layout
-from igraph.drawing.colors import palettes
+from igraph.drawing import colors
 import numpy as np
 from scipy import spatial
 from sklearn.cluster import DBSCAN
@@ -75,15 +75,20 @@ def reduce_with_linloglayout(g, root_user):
         layout_list.append((x_value, y_value))
     return layout_list
 
-def id_communities(g, layout_list):
+def id_communities(g, layout_list, eps=0.42, min_samples=10):
     layout_distance = spatial.distance.squareform(spatial.distance.pdist(layout_list))
     layout_similarity = 1 - (layout_distance / np.max(layout_distance))
-    community_idx_list = DBSCAN().fit(layout_similarity, eps=0.42, min_samples=10).labels_
+    community_idx_list = DBSCAN().fit(layout_similarity, eps=eps, min_samples=min_samples).labels_
     community_idx_list = [int(x) for x in community_idx_list]
     if -1 in community_idx_list:
         community_idx_list = list(np.array(community_idx_list) + 1)
-    #set color based on community
-    g.vs['color'] = list(int(256 / len(set(community_idx_list))) * np.array(community_idx_list))
+
+    #set color based on community (255 is white)
+    color_step = 256 / len(set(community_idx_list))
+    colors = np.array(community_idx_list) * color_step
+    colors = 255 - colors
+    g.vs['color'] = list(colors)
+    g.vs['shape'] = ['hidden' if x == 0 else 'circle' for x in community_idx_list]
     return g, community_idx_list
 
 
@@ -106,35 +111,36 @@ if __name__ == "__main__":
     network = TwitterUser.get_rooted_network(root_user, postgres_handle, distance=distance)
     g = get_igraph_graph(network)
     layout_list = reduce_with_linloglayout(g, root_user)
-    g, community_idx_list = id_communities(g, layout_list)
     layout = Layout(layout_list)
+
+    g, community_idx_list = id_communities(g, layout_list, eps=0.72, min_samples=10)
     vertex_clustering = VertexClustering(g, community_idx_list)
 
     #palettes
     #  'red-yellow-green','gray','red-purple-blue','rainbow',
     #  'red-black-green','terrain','red-blue','heat','red-green'
-    plot(g, layout=layout, edge_color="white", palette=palettes["rainbow"])
+    plot(g, layout=layout, edge_color="white", palette=colors.palettes["rainbow"], vertex_order_by=('size', True))
 
-    # communities = defaultdict(lambda: [[], [], []])
-    # i = 1
-    # for community_graph in vertex_clustering.subgraphs():
-    #     communities[i][1] = community_graph.vs['name']
-    #     communities[i][2] = community_graph.vs['size']
-    #     i += 1
+    communities = defaultdict(lambda: [[], [], []])
+    i = 1
+    for community_graph in vertex_clustering.subgraphs():
+        communities[i][1] = community_graph.vs['name']
+        communities[i][2] = community_graph.vs['size']
+        i += 1
 
-    # print 'save final to disk'
-    # twitter_reduction = TwitterReduction.create_reduction(root_user.id, postgres_handle)
-    # postgres_handle.connection.commit()
-    # for community_idx, id_rank_tup in communities.items():
-    #     #params:
-    #     #reduction_id, index, 
-    #     #community_edges, member_ids, member_scores, postgres_handle
-    #     if len(id_rank_tup[2]) > 5:
-    #         TwitterCommunity.create_community(twitter_reduction.id, community_idx, 
-    #             id_rank_tup[0], id_rank_tup[1], id_rank_tup[2], postgres_handle)
-    #     postgres_handle.connection.commit()
-    # TwitterCommunity.mk_tag_clouds(twitter_reduction.id, postgres_handle)
-    # postgres_handle.connection.commit()
+    print 'save to disk'
+    twitter_reduction = TwitterReduction.create_reduction(root_user.id, postgres_handle)
+    postgres_handle.connection.commit()
+    for community_idx, id_rank_tup in communities.items():
+        #params:
+        #reduction_id, index, 
+        #community_edges, member_ids, member_scores, postgres_handle
+        if community_idx > 0:
+            TwitterCommunity.create_community(twitter_reduction.id, community_idx, 
+                id_rank_tup[0], id_rank_tup[1], id_rank_tup[2], postgres_handle)
+        postgres_handle.connection.commit()
+    TwitterCommunity.mk_tag_clouds(twitter_reduction.id, postgres_handle)
+    postgres_handle.connection.commit()
 
     print datetime.now() - start_time
 
