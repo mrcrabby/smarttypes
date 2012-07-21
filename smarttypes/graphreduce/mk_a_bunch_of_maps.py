71e79f0821da032981171a3bd1b6387c4dac83cb
Timmyt Wilson <hello@smarttypes.org>
2012-07-21 16:14:18 -0400

    blah

--- smarttypes/graphreduce/mk_a_bunch_of_maps.py
+++ smarttypes/graphreduce/mk_a_bunch_of_maps.py
@@ -1,3 +1,5 @@
+
+import os
 import smarttypes
 from smarttypes.model.twitter_user import TwitterUser
 from smarttypes.model.twitter_reduction import TwitterReduction
@@ -5,4 +7,9 @@ from smarttypes.model.twitter_community import TwitterCommunity
 
 
 if __name__ == "__main__":
+
+	root_user_count_tups = TwitterReduction.get_user_reduction_counts(postgres_handle)
+	for user, reduction_count in root_user_count_tups:
+		print 'making map for %s' % user.screen_name
+		os.system('python reduce_graph.py %s 0' % user.screen_name)
 
