658b6fab6eabafb9aa16c66766cf4593d1dc9bed
Timmyt Wilson <hello@smarttypes.org>
2012-07-29 09:31:34 -0400

    blah

--- smarttypes/scripts/backup_db.sh
+++ smarttypes/scripts/backup_db.sh
@@ -1,5 +1,7 @@
 
-pg_dump smarttypes | gzip > /data/st.`/bin/date +"%Y_%m_%d"`.gz
+filepath='/data/st.'`/bin/date +"%Y_%m_%d"`'.dump'
+pg_dump -f ${filepath} --format=custom smarttypes
+#pg_dump smarttypes | gzip > /data/st.`/bin/date +"%Y_%m_%d"`.gz
 
 
 
