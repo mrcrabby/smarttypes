468acb7f9f41dff81bebce6e5088442facb47aa5
Timmyt Wilson <hello@smarttypes.org>
2012-08-21 07:08:33 -0400

    locations query

---
+++ smarttypes/sql/locations.sql
@@ -0,0 +1,10 @@
+
+\o locations.csv
+\f ','
+\a
+select count(location_name), replace(location_name, ',' ' ') 
+from twitter_user 
+group by location_name 
+order by count(location_name) desc;
+\o
+
