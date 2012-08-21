07f14a497d448af3a814cadd7edb0978627d2951
Timmyt Wilson <hello@smarttypes.org>
2012-08-21 07:10:53 -0400

    locations query

--- smarttypes/sql/locations.sql
+++ smarttypes/sql/locations.sql
@@ -2,7 +2,7 @@
 \o locations.csv
 \f ','
 \a
-select count(location_name), replace(location_name, ',' ' ') 
+select count(location_name), replace(location_name, ',', ' ') 
 from twitter_user 
 group by location_name 
 order by count(location_name) desc;
