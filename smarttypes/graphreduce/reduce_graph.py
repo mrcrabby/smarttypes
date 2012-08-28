2e5524570cb43a60ade26102ce775105a71bbe2f
Timmyt Wilson <hello@smarttypes.org>
2012-08-27 20:12:25 -0400

    blah

--- smarttypes/graphreduce/reduce_graph.py
+++ smarttypes/graphreduce/reduce_graph.py
@@ -113,7 +113,7 @@ def get_network_stats(network, g, vertex_clustering):
 
 def calculate_hybrid_pagerank(global_pagerank, community_pagerank, community_score):
     hybrid_pagerank = community_pagerank * community_score
-    hybrid_pagerank = hybrid_pagerank / scoreatpercentile(hybrid_pagerank, 97)
+    hybrid_pagerank = hybrid_pagerank / scoreatpercentile(hybrid_pagerank, 98)
     return hybrid_pagerank
 
 if __name__ == "__main__":
