d95c947b4f16126073b2f08ebb73d33bec9640fb
Timmyt Wilson <hello@smarttypes.org>
2012-08-29 06:47:21 -0400

    tweaking graph_reduce algo

--- smarttypes/graphreduce/reduce_graph.py
+++ smarttypes/graphreduce/reduce_graph.py
@@ -136,7 +136,7 @@ if __name__ == "__main__":
         distance = int(sys.argv[2])
     root_user = TwitterUser.by_screen_name(screen_name, postgres_handle)
     if distance < 1:
-        distance = 30000 / len(root_user.following[:1000])
+        distance = 25000 / len(root_user.following[:1000])
 
     #get network and reduce
     if smarttypes.config.IS_PROD:
