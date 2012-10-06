27e910240060892c0473c0810394bad9cc00f128
Timmyt Wilson <hello@smarttypes.org>
2012-10-06 15:44:53 -0400

    working on node movement

--- smarttypes/graphreduce/reduce_big_graph.py
+++ smarttypes/graphreduce/reduce_big_graph.py
@@ -4,12 +4,26 @@ import numpy as np
 from collections import OrderedDict
 
 
-
 def figure_out_everyones_next_move(network, coordinates_as_dict, coordinates_as_numpy_array):
 
+    """
+    avg_position_of_all_nodes
+    avg_position_of_followie_nodes
+    """
+    avg_position_of_all_nodes = np.mean(coordinates_as_numpy_array, 0)
+
     new_coordinates_as_dict = OrderedDict()
+    for node_id in network:
+        followie_coordinates = []
+        for followie_id in network[node_id]:
+            followie_coordinates.append(coordinates_as_dict[followie_id])
+        followie_coordinates = np.array(followie_coordinates)
+        avg_position_of_followie_nodes = np.mean(followie_coordinates, 0)
+        #make a move
+        
 
     return new_coordinates_as_dict
+
 
 def how_are_we_doing_compared_to_our_objective(network, coordinates_as_dict, coordinates_as_numpy_array):
 
@@ -18,16 +32,15 @@ def how_are_we_doing_compared_to_our_objective(network, coordinates_as_dict, coo
     """
 
 
-
 def reduce_with_semi_intelligent_agents(network):
     #network is an ordereddict
 
     #randomly assign coordinates
     coordinates_as_dict = OrderedDict()
-    for node in network:
+    for node_id in network:
         x = random.random() * 100
         y = random.random() * 100
-        coordinates_as_dict = (x, y)
+        coordinates_as_dict[node_id] = (x, y)
     coordinates_as_numpy_array = np.array(coordinates_as_dict.values())
 
     for i in range(10):
--- smarttypes/graphreduce/test_objective_function.py
+++ smarttypes/graphreduce/test_objective_function.py
@@ -1,38 +1,58 @@
 
 from optparse import OptionParser
-import math
+import numpy as np
 
 """
-given 2 points (one good and one bad) 
-show how our objective function ranks 
-various spots between good and bad points
-"""
+given 3 cartesian points (good, bad, me) prove that we can:
 
+  + id the best goal point
+    - equals goal_to_good_dist
+	- on the same line as good and bad
+	- (goal to bad distance) > (good to bad distance)
+
+  + id my movement (some step towards our goal point)
+
+"""
 
 if __name__ == '__main__':
 
-	parser = OptionParser()
-	parser.add_option("-g", "--good", action="store", type="int", dest="good")
-	parser.add_option("-b", "--bad", action="store", type="int", dest="bad")
-	options, args = parser.parse_args()
+	goal_to_good_dist = 0.25
+	good = np.array((1.2, 1.1))
+	bad = np.array((-4.4, -3.7))
+	me = np.array((7.4, -3.3))
 
-	scores = []
-	for x in range(20):
-		good_dist = max(abs(options.good - x), 2)
-		bad_dist = max(abs(options.bad - x), 2)
-		score = float(math.log(good_dist)) / float(bad_dist)
-		values = (score, x, good_dist, bad_dist)
-		scores.append(values)
+	good_to_bad_diff = good - bad 
+	good_to_bad_dist = np.linalg.norm(good_to_bad_diff)
+	good_to_bad_x_ratio = good_to_bad_diff[0] / good_to_bad_dist 
+	good_to_bad_y_ratio = good_to_bad_diff[1] / good_to_bad_dist 
 
-	for x in sorted(scores):
-		print "Score: %s, Point: %s, Good dist: %s, Bad dist: %s" % x
+	goal_to_bad_dist = good_to_bad_dist + goal_to_good_dist
+	goal_to_bad_x_dist = goal_to_bad_dist * good_to_bad_x_ratio
+	goal_to_bad_y_dist = goal_to_bad_dist * good_to_bad_y_ratio
+	goal_x = good[0] + (goal_to_bad_x_dist - good_to_bad_diff[0])
+	goal_y = good[1] + (goal_to_bad_y_dist - good_to_bad_diff[1])
+	goal = np.array((goal_x, goal_y))
+
+	assert np.linalg.norm(goal-bad) == goal_to_bad_dist, \
+		"""
+This thing doest work.
+ - np.linalg.norm(goal-bad): %s
+ - goal_to_bad_dist: %s
+ - good: %s
+ - bad: %s
+ - goal: %s
+ - good_to_bad_x_ratio: %s
+ - good_to_bad_y_ratio: %s
+		""" % (np.linalg.norm(goal-bad), goal_to_bad_dist, 
+			good, bad, goal, good_to_bad_x_ratio, good_to_bad_y_ratio)
 
 
-
-
-
-
-
-
-
+	new_x = (goal[0] + me[0]) * 0.25
+	new_y = (goal[1] + me[1]) * 0.25
+	new_spot = np.array((new_x, new_y))
+	print "bad: %s" % bad
+	print "good: %s" % good
+	print "goal: %s" % goal
+	print "me: %s" % me
+	print "new_spot: %s" % new_spot
 
